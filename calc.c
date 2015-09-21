0b43f2a62f1f18e9e27cf20ce613e1bef46e1942
rgupta1220 <rgupta1220@ymail.com>
2015-09-21 16:33:27 +0530

    Update calc.c
    added functionality of adding two num

--- calc.c
+++ calc.c
@@ -3,4 +3,15 @@
 void main
 {
 clrscr();
+int num1,num2,res;
+char op;
+printf("Enter num1");
+scanf("%d",&num1);
+printf("Enter num2");
+scanf("%d",&num2);
+printf("Enter operator");
+scanf("%c",&op);
+if(op=="+")
+res=add(num1,num2);
+printf("result=%d",res);
 }
