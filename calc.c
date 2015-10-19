51aa0793a1950972d765637aad2665e2a66f556b
rahul <rgupta1220@ymail.com>
2015-10-19 16:52:01 +0530

    new branch

--- calc.c
+++ calc.c
@@ -1,17 +1,21 @@
 #include<stdio.h>
 #include<conio.h>
+
 int add(int a,int b)
 {
   return a+b;
 }
+
 int sub(int a,int b)
 {
 return a-b;
 }
+
 int mul(int a,int b)
 {
 return a*b;
 }
+
 int div(int a,int b)
 {
 return a/b;
